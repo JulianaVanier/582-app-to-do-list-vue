2b92c66618cf37adc1b4dbba3f1c540a91de32d3
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-08-08 15:37:10 -0400

    added some data for start code

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,68 @@ import ListOfLists from "./components/ListOfLists.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      lists: [
+        {
+          id: 1,
+          name: "List 1",
+        },
+        {
+          id: 2,
+          name: "List 2",
+        },
+        {
+          id: 3,
+          name: "List 3",
+        }
+      ],
+      tasks: [
+        {
+          id: 1,
+          name: "Task 1",
+          date: "2023-01-01",
+          responsability: "Person 1",
+          list_id: 1,
+        },
+        {
+          id: 2,
+          name: "Task 2",
+          date: "2023-01-02",
+          responsability: "Person 2",
+          list_id: 2,
+        },
+        {
+          id: 3,
+          name: "Task 3",
+          date: "2023-01-03",
+          responsability: "Person 3",
+          list_id: 2,
+        },
+        {
+          id: 4,
+          name: "Task 4",
+          date: "2023-01-04",
+          responsability: "Person 4",
+          list_id: 3,
+        },
+        {
+          id: 5,
+          name: "Task 5",
+          date: "2023-01-05",
+          responsability: "Person 5",
+          list_id: 3,
+        },
+        {
+          id: 6,
+          name: "Task 6",
+          date: "2023-01-06",
+          responsability: "Person 6",
+          list_id: 3,
+        },
+      ]
+    };
+  },
   components: {
     // HelloWorld,
     ListOfLists,
