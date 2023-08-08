6cc706f397c0942f827c12f804ec1c9ccf676be5
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-08-08 16:21:40 -0400

    created test TaskItem: renders props tasks

--- tests/unit/taskItem.spec.js
+++ tests/unit/taskItem.spec.js
@@ -1,14 +1,28 @@
-// import { shallowMount } from "@vue/test-utils";
-// import TaskItem from "@/components/TaskItem";
+import { shallowMount } from "@vue/test-utils";
+import TaskItem from "@/components/TaskItem";
 
-// describe("TaskItem.vue", () => {
-//   // Props
-//   it("renders props.tasks when passed", () => {});
+describe("TaskItem.vue", () => {
+  // Props
+  it("renders props.tasks when passed", () => {
+    const tasks = {
+      id: 1,
+      name: "Task 1",
+      date: "2023-01-01",
+      responsability: "Person 1",
+      list_id: 1,
+    };
+    const wrapper = shallowMount(TaskItem, {
+      props: { tasks },
+    });
 
-//   // Emit
-//   it("emit an addImportant event", () => {});
+    expect(wrapper.find("h2").text()).toBe(tasks.name);
+    // expect(wrapper.text()).toMatch([task.name]);
+  });
 
-//   it("emit an addToday event", () => {});
+  // Emit
+  //   it("emit an addImportant event", () => {});
 
-//   it("emit task id event", () => {});
-// });
+  //   it("emit an addToday event", () => {});
+
+  //   it("emit task id event", () => {});
+});
